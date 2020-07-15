3f08754a3622b4f0ea44c43e3ce96cdba1c35fb2
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-07-15 19:01:47 +0200

    error code

--- js/app.js
+++ js/app.js
@@ -1,76 +1,13 @@
-let sections = document.querySelectorAll("section");
-const navbar = document.getElementById("navbar_list");
 
-// Dynamic navbar list and add class to menu 
-function createNavbar() {
-  for (let i = 0; i < sections.length; i++) {
-    const section = sections[i];
-    const liList = document.createElement("li");
-    let sectionLink = document.createElement("A");
+var menuLists = document.getElementById("menuList");
 
-    liList.setAttribute("id", section.dataset.nav);
-    sectionLink.setAttribute("href", `#${sections[i].id}`);
-    sectionLink.textContent = `${sections[i].id}`;
-    sectionLink.classList.add("menu_link");
 
-    liList.classList.add("links");
+var listItems = menuLists.getElementsByClassName("listItem");
 
-    liList.appendChild(sectionLink);
-    navbar.appendChild(liList);
-  }
+for (var i = 0; i < listItems.length; i++) {
+  listItems[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
-createNavbar();
-
-// Add class 'active' 
-let landingSection = document.querySelector("div");
-let bounding = landingSection.getBoundingClientRect();
-
-let isInViewport = function(elem) {
-  let bounding = elem.getBoundingClientRect();
-  return (
-    bounding.top >= 0 &&
-    bounding.left >= 0 &&
-    bounding.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    bounding.right <=
-      (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
-
-window.addEventListener("scroll", function(event) {
-    if (isInViewport(landingSection)) {
-      landingSection.classList.add("active_class");
-    }
-  }, false);
-
-// Scroll to anchor 
-let anchorLinks = document.querySelectorAll('a[href^="#"]');
-
-for (let item of anchorLinks) {
-  item.addEventListener("click", e => {
-    let hashval = item.getAttribute("href");
-    let target = document.querySelector(hashval);
-    target.scrollIntoView({
-      behavior: "smooth",
-      block: "start"
-    });
-    history.pushState(null, null, hashval);
-    e.preventDefault();
-  });}
-
-
-  const links = document.querySelectorAll(".links");
-
-  function changeLinkState() {
-    let index = sections.length;
-  
-    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
-    links.forEach(link => link.classList.remove("active"));
-  
-    links[index].classList.add("active");
-  }
-  
-  changeLinkState();
-  
-  window.addEventListener("scroll", changeLinkState);
-  
