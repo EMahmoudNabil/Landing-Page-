1ae49dae11f371bdfb3d512cc233223ff8f48e01
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-07-15 21:40:51 +0200

    Edit to file js to Dynamic navbar & active state for this nav

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-## this is sample Landing page for taks in udacity 
-## this project exhit 4 section and easy to move between them 
-## the structure of the project like this 
-    css
-    - styles.css    
-    index.html
-    js
-    - app.js
-    README.md
+# Landing Page Project
+
+## Features
+* Create dynamic navbar menu using pure vanilla JavaScript.
+* Add active state to the section, it should be clear which section is being viewed while scrolling 
+through the page.
+* Check if the element is near the top of viewport if it is than add active class to the section.
+* Smooth Scroll while clicking on the anchor tag and it should take user to that section.
+
+## Author
+
+- **Mahmoud Nabil** - [EMahmoudNabil](https://github.com/EMahmoudNabil)
--- js/app.js
+++ js/app.js
@@ -1,13 +1,59 @@
 
-var menuLists = document.getElementById("menuList");
+const sections= document.getElementsByTagName('section');
+const navBarList = document.getElementById('navbar_list');
 
-
-var listItems = menuLists.getElementsByClassName("listItem");
-
-for (var i = 0; i < listItems.length; i++) {
-  listItems[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+// Dynamic unorder List
+// Dynamic navbar list and add class to menu 
+function createNavbar() {
+  const imaginaryMethode  = document.createDocumentFragment();
+  for (const section of sections) {
+      const IDSection = section.getAttribute('id');
+      const liNav = document.createElement('li');
+  
+      liNav.innerHTML = `<a href= #${IDSection} class=${'menu_link'}>${IDSection} </a>`;
+      liNav.classList.add("links");
+      imaginaryMethode .appendChild(liNav);
+  }
+  navBarList.appendChild(imaginaryMethode );
 }
+
+createNavbar();
+
+// Add class 'active
+function isInViewport() {
+  /* Delete Class*/
+  if (document.querySelector('section.active') != null &&
+      document.querySelector('a.active') != null) {
+        document.querySelector('section.active').removeAttribute('class');
+        document.querySelector('a.active').removeAttribute('class');
+  }
+}
+
+
+// modification of state link
+const links = document.querySelectorAll(".links");
+function changeState() {
+  let index = sections.length;
+
+  while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  links.forEach(link => link.classList.remove("active"));
+  links[index].classList.add("active");
+}
+
+changeState();
+
+//  Scroll to anchor ID using and smooth motion
+let scrollToSection = function (e) {
+  event.preventDefault();
+  const section = document.querySelector(`#${e.target.innerHTML}`);
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+window.addEventListener("scroll", function(e) {
+      inViewPort();
+  });
+
+  navBarList.addEventListener('click', scrollToSection);
+  window.addEventListener("scroll", changeState);
+
+  
