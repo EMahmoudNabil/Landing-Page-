364d2ad99a113a870b9c21179dbdc14bd87bd082
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-07-17 09:21:00 +0200

    final edit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
+// define varible 
 const navBarList = document.getElementById('navbar_list');
+
 // Dynamic unorder List
 // Dynamic navbar list and add class to menu 
 function DynamicNav() {
   const allSections= document.getElementsByTagName('section');
-  const imaginaryMethode  = document.createDocumentFragment();
+  const imaginaryMethod  = document.createDocumentFragment();
   for (const section of allSections) {
       const IDSection = section.getAttribute('id');
       const liNav = document.createElement('li');
       liNav.innerHTML = `<a href= #${IDSection} class=${'dynmicLink'}>${IDSection} </a>`;
-      liNav.classList.add("links");
-      imaginaryMethode .appendChild(liNav);
+      liNav.classList.add("listItem");
+      imaginaryMethod.appendChild(liNav);
   }
-  navBarList.appendChild(imaginaryMethode );
+  navBarList.appendChild(imaginaryMethod );
 }
 DynamicNav();
 
@@ -23,12 +25,22 @@ let checkView = function() {
         document.querySelector('section.active').removeAttribute('class');
         document.querySelector('a.active').removeAttribute('class');
   }
-
     }
 
-// change state { ############ }
+// define varible 
+const getLinks = document.querySelectorAll(".listItem");
+const allSections= document.getElementsByTagName('section');
+// modification of state link
+function changeState() {
+  let getIndex = allSections.length;
+  while (--getIndex && window.scrollY < allSections[getIndex].offsetTop) {}
+    getLinks[getIndex].classList.add("active");
+  getLinks.forEach(listItem => listItem.classList.remove("active"));
+  getLinks[getIndex].classList.add("active");
+}
 
-//  Scroll to anchor ID using and smooth motion
+
+//  Scrolling Smooth
 let scrollingSection = function (e) {
   event.preventDefault();
   const section = document.querySelector(`#${e.target.innerHTML}`);
